9d5c09274d90383bdc23ede40d68dbc2e5b137b2
physics111232 <168702816+physics111232@users.noreply.github.com>
2024-05-31 16:07:11 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ echo "Updated by llbranco"
 interface=eth0
 firmware=1100
 stage1=/media/internal/downloads/PPLGPwn-main/stage1.bin
-stage2=/media/internal/downloads/PPLGPwn-main/stage2.bin
+stage2=/media/internal/downloads/PPLGPwn-main/stage2_11.00.bin
 cd /media/internal/downloads/PPLGPwn-main
 chmod +x ./pppwn
 
