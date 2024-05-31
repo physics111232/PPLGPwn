38f85c958ea0a006c8ee3a42d195a6329e868350
physics111232 <168702816+physics111232@users.noreply.github.com>
2024-05-31 12:10:29 -0700

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,25 @@
+echo "PPLGPWN - Designed for lgwebtvs! Ported by Kodeine, with luv <3"
+echo "Updated by llbranco"
+interface=eth0
+firmware=1100
+stage1=/media/internal/downloads/PPLGPwn-main/stage1.bin
+stage2=/media/internal/downloads/PPLGPwn-main/stage2_11.00.bin
+cd /media/internal/downloads/PPLGPwn-main
+chmod +x ./pppwn
+
+# send notification
+luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn!</b><br/>Starting PS4 Jailbreak."}'
+
+# retry logic
+while true; do
+    ./pppwn --interface $interface --fw $firmware --stage1 $stage1 --stage2 $stage2 --auto-retry
+    RETRY_CODE=$?
+    if [ $RETRY_CODE -eq 0 ]; then
+        break
+    fi
+    # send retry notification
+    luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>Retrying PS4 Jailbreak...</b>"}'
+done
+
+# send success notification
+luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn!</b><br/>PS4 Jailbreak Success!!. <br/>"}'
