f52d8671b808cb6ee53cf29fc898aa3d6d2e3d62
physics111232 <168702816+physics111232@users.noreply.github.com>
2024-05-31 17:27:40 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,9 +7,19 @@ stage2=/media/internal/downloads/PPLGPwn-main/stage2_11.00.bin
 cd /media/internal/downloads/PPLGPwn-main
 chmod +x ./pppwn
 
-# send nofification
-luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>Starting Jailbreak..</b>"}'
+# send notification
+luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn!</b><br/>Starting PS4 Jailbreak."}'
 
-./pppwn --interface $interface --fw $firmware --stage1 $stage1 --stage2 $stage2 --auto-retry
+# retry logic
+while true; do
+    ./pppwn --interface $interface --fw $firmware --stage1 $stage1 --stage2 $stage2 --auto-retry
+    RETRY_CODE=$?
+    if [ $RETRY_CODE -eq 0 ]; then
+        break
+    fi
+    # send retry notification
+    luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>Retrying PS4 Jailbreak...</b>"}'
+done
 
-luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>SUCCESSFULLY JAILBROKEN!</b>"}'
+# send success notification
+luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>PPLGPwn!</b><br/>PS4 Jailbreak Success!!. <br/>"}'
