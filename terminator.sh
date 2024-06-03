903321dcf0309bd006e2d806d839b974cc497b82
physics111232 <168702816+physics111232@users.noreply.github.com>
2024-06-02 20:27:33 -0700

    Update terminator.sh

--- terminator.sh
+++ terminator.sh
@@ -2,4 +2,4 @@ kill $(ps aux | grep '[p]ppwn' | awk '{print $2}')
 kill $(ps aux | grep '[s]tage1' | awk '{print $2}')
 kill $(ps aux | grep '[s]tage2_11.00' | awk '{print $2}')
 kill $(ps aux | grep '[s]tart' | awk '{print $2}')
-luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>Terminated All Runnning Scripts</b>"}'
+luna-send -f -n 1 luna://com.webos.notification/createToast '{"message": "<b>Terminated All Running Scripts</b>"}'
