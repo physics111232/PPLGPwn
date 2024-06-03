88dcf5bef03d37b2efbaac7e27ec4bb414224e8f
physics111232 <168702816+physics111232@users.noreply.github.com>
2024-06-02 20:16:00 -0700

    Update terminator.sh

--- terminator.sh
+++ terminator.sh
@@ -1,2 +1,4 @@
-kill $(ps aux | grep '[t]est1' | awk '{print $2}')
-kill $(ps aux | grep '[t]est2' | awk '{print $2}')
+kill $(ps aux | grep '[p]ppwn' | awk '{print $2}')
+kill $(ps aux | grep '[s]tage1' | awk '{print $2}')
+kill $(ps aux | grep '[s]tage2_11.00' | awk '{print $2}')
+kill $(ps aux | grep '[s]tart' | awk '{print $2}')
