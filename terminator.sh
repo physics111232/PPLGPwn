931c473ccf82ce8481e606a9e6c4937996217c81
physics111232 <168702816+physics111232@users.noreply.github.com>
2024-06-02 19:24:16 -0700

    Update terminator.sh

--- terminator.sh
+++ terminator.sh
@@ -4,17 +4,26 @@
 SCRIPT1="/media/internal/downloads/test-message/test1.sh"
 SCRIPT2="/media/internal/downloads/test-message/test2.sh"
 
-# Get the PIDs of the running scripts
-PIDS=$(ps -ef | grep -E "$SCRIPT1|$SCRIPT2" | grep -v grep | awk '{print $2}')
+# Function to kill processes by script name
+kill_script_processes() {
+    local script_path=$1
+    local pids=$(pgrep -f "$script_path")
+    
+    if [ -z "$pids" ]; then
+        echo "No matching processes found for $script_path."
+    else
+        echo "Killing the following PIDs for $script_path: $pids"
+        for pid in $pids; do
+            kill -9 $pid
+            if [ $? -eq 0 ]; then
+                echo "Successfully killed process $pid"
+            else
+                echo "Failed to kill process $pid"
+            fi
+        done
+    fi
+}
 
-# Check if any PIDs were found
-if [ -z "$PIDS" ]; then
-    echo "No matching scripts are currently running."
-else
-    # Kill the processes
-    echo "Killing the following PIDs: $PIDS"
-    for PID in $PIDS; do
-        kill -9 $PID
-        echo "Killed process $PID"
-    done
-fi
+# Kill processes for each script
+kill_script_processes "$SCRIPT1"
+kill_script_processes "$SCRIPT2"
